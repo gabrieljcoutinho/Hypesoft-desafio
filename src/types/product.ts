51cf63a33a3505407249011897ea5b4948d61aad
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-26 20:30:05 -0300

    teste

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,10 +1,9 @@
-// src/types/product.ts
 export interface Product {
   id: string;
   name: string;
   description: string;
   price: number;
-  category: string;
-  quantity: number;
+  categoryId: string;    // Nome correto conforme seu código da página
+  stockQuantity: number; // Nome correto conforme seu código da página
   createdAt: string;
 }
