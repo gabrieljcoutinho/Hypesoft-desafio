26531d7112a829a5a48c0ff87ee893c2cdf499b4
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-26 20:34:20 -0300

    tes

--- src/types/product.ts
+++
@@ -1,9 +0,0 @@
-export interface Product {
-  id: string;
-  name: string;
-  description: string;
-  price: number;
-  categoryId: string;    // Nome correto conforme seu código da página
-  stockQuantity: number; // Nome correto conforme seu código da página
-  createdAt: string;
-}
