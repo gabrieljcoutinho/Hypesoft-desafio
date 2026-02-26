c640036e72aff36e31240966dafaf3cb9d8274bc
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-26 11:31:50 -0300

    arquivo

---
+++ src/types/product.ts
@@ -0,0 +1,14 @@
+export interface Category {
+    id: string;
+    name: string;
+  }
+
+  export interface Product {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    category: Category;
+    categoryId: string;
+    stockQuantity: number;
+  }
