7eb84d90e45d32f6aeae8d92c0cf09f4e0006f2b
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-26 20:14:23 -0300

    conteudo

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // ADICIONADO ISSO
 import App from './App';
 import './index.css';
 import keycloak from './services/keycloak';
 
 keycloak.init({
-  onLoad: 'login-required', // Isso obriga o login logo de cara
-  checkLoginIframe: false
+  onLoad: 'login-required',
+  checkLoginIframe: false,
+  pkceMethod: 'S256' // Recomendado para evitar erros de segurança
 }).then((authenticated) => {
   if (authenticated) {
     ReactDOM.createRoot(document.getElementById('root')!).render(
       <React.StrictMode>
-        <App />
+        {/* O BrowserRouter deve abraçar o App */}
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </React.StrictMode>
     );
   }
